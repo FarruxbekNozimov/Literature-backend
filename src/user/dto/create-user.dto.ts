8a94debb5017647f018fc0f04ec184328ebb54ce
FarruxDEV <farruxbeknozimov@gmail.com>
2023-09-07 08:02:33 +0500

    add api property to user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,33 @@
+import {
+  IsEmail,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'John' })
+  @IsString()
   first_name: string;
-	last_name: string;
-	phone: string;
-	email: string;
-	password: string;
-	user_photo: string;
-	
+
+  @ApiProperty({ example: 'Doe' })
+  @IsString()
+  last_name: string;
+
+  @ApiProperty({ example: '+998887038006' })
+  @IsPhoneNumber()
+  phone: string;
+
+  @ApiProperty({ example: 'email@gmail.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'Uzb@kis!0n' })
+  @IsStrongPassword()
+  password: string;
+
+  @ApiProperty({ example: 'HEQJLAM.png' })
+  @IsString()
+  user_photo: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,21 @@
-export class UpdateUserDto {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'John' })
   first_name?: string;
-	last_name?: string;
-	phone?: string;
-	email?: string;
-	password?: string;
-	user_photo?: string;
-	
+
+  @ApiProperty({ example: 'Doe' })
+  last_name?: string;
+
+  @ApiProperty({ example: '+998887038006' })
+  phone?: string;
+
+  @ApiProperty({ example: 'email@gmail.com' })
+  email?: string;
+
+  @ApiProperty({ example: 'Uzb@kis!0n' })
+  password?: string;
+
+  @ApiProperty({ example: 'HEQJLAM.png' })
+  user_photo?: string;
 }
